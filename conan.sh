a51b7cd0e0b5ec890130a8fe3b3e04cec193a294
shubhgkr <shubhgkr@gmail.com>
2018-08-15 12:51:35 +0530

    patch fix for conan.sh file to enable filenames based on episode number

--- conan.sh
+++ conan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-value=`cat /home/user/Desktop/count.txt`
+value=`cat ~/conan/count.txt`
 count=${value##*$'\n'}
 count=$(($count+1))
 echo $count
@@ -10,6 +10,6 @@ result="`wget -qO- $url`"
 #echo $result|grep -Po "source src=\"http.*?video\.mp4"|head -n 1 |grep -o "h.*"
 url=`echo $result|grep -Po "source src=\"http.*?video\.mp4" | head -n 1 | sed 's/source src="//'`
 echo $url
-#youtube-dl $url --ignore-config -o ~/%(title)s.%(ext)s --no-check-certificate
-echo -e $count>>"count.txt"
-echo "done"
+output='/home/shubhgkr/Videos/ep'$count'.%(ext)s'
+youtube-dl $url --ignore-config --console-title -o $output --no-check-certificate
+echo $count>>"count.txt"
